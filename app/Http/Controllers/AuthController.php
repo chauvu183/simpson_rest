ac9bb8711a584f201a1e44115333ede33c8b08c5
chauvu183 <kiwill.vu@gmail.com>
2023-08-14 22:25:06 +0200

    remove unused swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,46 +9,6 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends BaseController
 {
-    /**
-     * @OA\Post(
-     * path="/api/register",
-     * operationId="Register",
-     * tags={"Register"},
-     * summary="User Register",
-     * description="User Register here",
-     *     @OA\RequestBody(
-     *         @OA\JsonContent(),
-     *         @OA\MediaType(
-     *            mediaType="multipart/form-data",
-     *            @OA\Schema(
-     *               type="object",
-     *               required={"name","email", "password", "password_confirmation"},
-     *               @OA\Property(property="name", type="text"),
-     *               @OA\Property(property="email", type="text"),
-     *               @OA\Property(property="password", type="password"),
-     *               @OA\Property(property="password_confirmation", type="password")
-     *            ),
-     *        ),
-     *    ),
-     *      @OA\Response(
-     *          response=201,
-     *          description="Register Successfully",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Register Successfully",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(
-     *          response=422,
-     *          description="Unprocessable Entity",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(response=400, description="Bad request"),
-     *      @OA\Response(response=404, description="Resource Not Found"),
-     * )
-     */
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -70,44 +30,6 @@ class AuthController extends BaseController
         return $this->sendResponse($success, 'User register successfully.');
     }
 
-    /**
-     * @OA\Post(
-     * path="/api/login",
-     * operationId="authLogin",
-     * tags={"Login"},
-     * summary="User Login",
-     * description="Login User Here",
-     *     @OA\RequestBody(
-     *         @OA\JsonContent(),
-     *         @OA\MediaType(
-     *            mediaType="multipart/form-data",
-     *            @OA\Schema(
-     *               type="object",
-     *               required={"email", "password"},
-     *               @OA\Property(property="email", type="email"),
-     *               @OA\Property(property="password", type="password")
-     *            ),
-     *        ),
-     *    ),
-     *      @OA\Response(
-     *          response=201,
-     *          description="Login Successfully",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Login Successfully",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(
-     *          response=422,
-     *          description="Unprocessable Entity",
-     *          @OA\JsonContent()
-     *       ),
-     *      @OA\Response(response=400, description="Bad request"),
-     *      @OA\Response(response=404, description="Resource Not Found"),
-     * )
-     */
     public function login(Request $request)
     {
         if(Auth::attempt(['name' => $request->name, 'password' => $request->password])){
